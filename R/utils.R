c19f28784f8f11f3a32c1bb02600b6e25d093708
Jessica Minnier <jessica.minnier@gmail.com>
2018-02-26 14:28:38 -0800

    import le pipe

---
+++ R/utils.R
@@ -0,0 +1,17 @@
+# Copied from tidyr/R/utils.R (and janitor and infer), to export the magrittr pipe
+
+#' Pipe operator
+#'
+#' @description Exported from the magrittr package.  To learn more, run \code{?magrittr::`\%>\%`}.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom dplyr %>%
+#' @usage lhs \%>\% rhs
+#' @examples
+#' mtcars %>%
+#'   tabyl(carb, cyl) %>%
+#'   adorn_totals()
+NULL
